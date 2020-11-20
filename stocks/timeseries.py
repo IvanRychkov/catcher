c975c4d6d14805ffcf46d0352e4259c15a870d3d
Ivan Rychkov <rychyrych@yandex.com>
2020-11-21 00:45:34 +0300

    working days updated to working hours

--- stocks/timeseries.py
+++ stocks/timeseries.py
@@ -1,8 +1,10 @@
-def working_days(data, datetime_col=None, weekend_days=(5, 6)):
+def working_hours(data, datetime_col=None, weekend_days=(5, 6)):
     """Оставляет только будние дни. По умолчанию использует индекс в качестве столбца с датой."""
     if not datetime_col:
-        datetime_col = data.index
-    return data[~datetime_col.dayofweek.isin(weekend_days)]
+        datetime_col = data.dropna().index
+    return data.dropna()[(~datetime_col.dayofweek.isin(weekend_days)) &
+                (datetime_col.hour.min() <= datetime_col.hour) &
+                (datetime_col.hour <= datetime_col.hour.max())]
 
 
-__all__ = ['working_days']
+__all__ = ['working_hours']
