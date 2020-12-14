e7543f638c042115ad708714d06e2902b261d521
Ivan Rychkov <rychyrych@yandex.com>
2020-12-14 04:24:04 +0300

    added some timeseries utils

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stocks-ivanrychkov",
-    version="0.0.3",
+    version="0.0.4",
     author="Ivan Rychkov",
     author_email="rychyrych@yandex.ru",
     description="ETL library for downloading stocks data.",
