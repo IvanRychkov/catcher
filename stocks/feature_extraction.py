ddd412ee459590ed459830e1d69077a346d30216
Ivan Rychkov <rychyrych@yandex.com>
2020-12-20 15:46:31 +0300

    tinkoff added, iex deprecated

--- stocks/feature_extraction.py
+++ stocks/feature_extraction.py
@@ -1,8 +1,5 @@
 import pandas as pd
-from tqdm import trange
-from tqdm.notebook import tnrange
-from .timeseries import last_day, future_periods
-from functools import partial
+from .timeseries import future_periods
 
 
 def lookahead_window(column, aggfunc, window_size=60, shift=0, **agg_kws):
@@ -32,68 +29,21 @@ def profit_chance_lookahead(window, **profit_kws):
         if future.shape[0] > 0 else 0
 
 
-def profit_chance(window, single_day=False, **profit_kws):
-    """Возвращает вероятность прибыли в окне.
-    single_day - ограничивает окно окончанием торгового дня."""
-    assert isinstance(window.index, pd.DatetimeIndex), 'Not a time series.'
-    # Сейчас 1 элемент
-    now = window.index[0]
-    # Если один день, то ограничиваем сегодняшним днём
-    if single_day:
-        future = window[window.index.dayofyear == now.dayofyear][1:]
-    # Либо всё дальнейшее время
-    else:
-        future = window[1:]
-    return (profit(window[now], future, as_bool=True, **profit_kws).sum() / future.shape[0])\
-        if future.shape[0] > 0 else 0
+def buy_recommendation(price_column, lookahead=120):
+    """Генерирует рекомендацию к покупке."""
+    return lookahead_window(price_column, profit_chance_lookahead, window_size=lookahead, broker_commission=0.0005)
 
 
-def lookahead_agg(column, aggfunc, window_size=60, drop_leakage=False, pbar='notebook', **agg_kws):
-    """Выполняет агрегирование столбца с забеганием вперёд.
-    pbar = {'notebook', None/False}."""
-
-    # Выбираем полоску прогресса
-    if pbar:
-        if isinstance(pbar, str):
-            if pbar.startswith('n'):
-                range_func = tnrange
-        else:
-            range_func = trange
-    else:
-        range_func = range
-
-    length = column.shape[0]
-    # Возвращаем агрегированные значения в скользящем окне
-    result = pd.Series(
-        data=(aggfunc(
-            column[i:(i + window_size) if window_size > 0 else None], **agg_kws)
-            for i in range_func(length)),
-        index=column.index)
-    # Если отбрасываем утечку данных, то удаляем window_size последних индексов
-    return result[:-window_size] if drop_leakage else result
-
-
-make_probs = partial(lookahead_agg, aggfunc=profit_chance, broker_commission=0.0005)
-
-
-def generate_features(data, future=True, rolling_periods=60):
+def generate_features(data, price_column, future=True, rolling_periods=60):
     """Пайплайн для генерации признаков."""
-    df = data.copy()
+    df = data[[price_column]]
     # Отсчёт
     if future:
         df['future'] = future_periods(data)
     # Среднее
     if rolling_periods > 0:
-        df[f'rolling_avg_{rolling_periods}'] = data.average.rolling(rolling_periods, min_periods=1).mean()
+        df[f'rolling_avg_{rolling_periods}'] = data[price_column].rolling(rolling_periods, min_periods=1).mean()
     return df
 
 
-def make_train_data(data, n_last=0, lookahead=60):
-    """Возвращает X и y учебной выборки."""
-    actual_data = last_day(data)
-    return generate_features(actual_data).iloc[-n_last:], make_probs(actual_data.average,
-                                                                     window_size=lookahead,
-                                                                     pbar=False).iloc[-n_last:]
-
-
-__all__ = ['lookahead_agg', 'profit', 'profit_chance']
+__all__ = ['buy_recommendation', 'lookahead_window', 'profit', 'profit_chance_lookahead']
